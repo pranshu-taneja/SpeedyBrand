039ae0a8574f86371d0e0aa15b586df6592c2e80
pranshu-taneja <pranshu45taneja@gmail.com>
2023-06-14 23:14:43 +0530

    Added Remote MongoDb connection✅

--- Server/index.ts
+++ Server/index.ts
@@ -45,7 +45,7 @@ app.get("/gettopics", async(req,res)=>{
 
 
 app.listen(port, async () => {
-    await mongoose.connect("mongodb://localhost:27017").then(() => {
+    await mongoose.connect(`${process.env.MONGO_URL}`).then(() => {
         console.log("mongodb Connected successfully");
         console.log(`Server is listening on port ${port}`);
     })
