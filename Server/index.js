0dda64847454d1e40ef0cbf2f436f1f36c83179f
pranshu-taneja <pranshu45taneja@gmail.com>
2023-06-15 19:11:27 +0530

    Integrated Chat Gpt API with the AiEditor✅

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import Topic from "./model/topic.js";
 import CORS from "cors"
-// import topic from "./model/topic.js";
+import {OpenAIApi, Configuration} from "openai"
 
 
 const app = Express()
@@ -12,7 +12,6 @@ dotenv.config();
 app.use(morgan('combined'));
 app.use(Express.json())
 app.use(CORS());
-
 
 app.get('/', (req, res) => {
   res.send("<h1>Javascript</h1>")
@@ -49,6 +48,44 @@ app.post("/deletetopic", async (req, res) => {
     })
   } catch (error) {
     res.status(500).send("Error occurred while deleting the topic.");
+  }
+});
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+app.post("/generate", async (req, res) => {
+  try {
+    const { prompt, tone } = await req.body;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `
+              Write On the following topic:
+              ${prompt} -- in a ${tone} tone   
+              ###
+            `,
+      max_tokens: 1000,
+      temperature: 0.5,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      // stop: ["\n"],
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: response.data.choices[0].text,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.response
+        ? error.response.data
+        : "There was an issue on the server",
+    });
   }
 });
 
